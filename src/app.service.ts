f2cceae60783b10540a9e31f20344e3ccd3107ec
Caio Buteri <caiobuteri10@gmail.com>
2022-12-06 11:07:44 -0300

    implementando redis

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient, User } from '@prisma/client';
 
+import { client } from './main';
 import { UserDTO } from './dto/user';
 
 const prisma = new PrismaClient();
-
 @Injectable()
 export class AppService {
   async createUser(user: UserDTO): Promise<string> {
@@ -16,10 +16,28 @@ export class AppService {
   }
 
   async getUsers(): Promise<User[]> {
-    return await prisma.user.findMany();
+    const usersFromCache = await client.get('usersList');
+    const isUsersFromCacheStale = !(await client.get('usersList:validation'));
+
+    if (isUsersFromCacheStale) {
+      const isRefetching = !!(await client.get('usersList:refetching'));
+
+      if (!isRefetching) {
+        await client.set('usersList:refetching', 'true', { EX: 20 });
+
+        const users = await prisma.user.findMany();
+        await client.set('usersList', JSON.stringify(users));
+        await client.set('usersList:validation', 'true', { EX: 5 });
+        await client.del('usersList:refetching');
+      }
+    }
+
+    return JSON.parse(usersFromCache);
   }
 
   async deleteUser(id: number): Promise<User> {
+    await client.del('usersList');
+
     return await prisma.user.delete({
       where: {
         id,
@@ -28,6 +46,8 @@ export class AppService {
   }
 
   async updateUser(id: number, user: User): Promise<User> {
+    await client.del('usersList');
+
     const oldUser = await prisma.user.findUnique({
       where: {
         id,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { createClient } from 'redis';
+
+export const client = createClient();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await client.connect();
   await app.listen(3000);
 }
+
 bootstrap();
