984cd428c21619f42310abae82181c55392d2d4f
Caio Buteri <caiobuteri10@gmail.com>
2022-12-06 08:53:14 -0300

    CRUD na tabela User

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,53 @@
-import { Controller, Get, Req, Res, Query, Param } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Put,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
 
 import { AppService } from './app.service';
+import { UserDTO } from './dto/user';
 
-interface ParamsProps {
+interface AlterUserProps {
   id: string;
 }
 
-@Controller()
+interface DeleteUserProps extends AlterUserProps {}
+
+interface PutUserProps extends AlterUserProps {}
+
+interface PutUserBodyProps {
+  user: User;
+}
+
+@Controller('user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:id')
-  getHello(@Req() request: Request, @Param() params: ParamsProps): string {
-    return this.appService.getHello(params.id);
+  @Post()
+  createUser(@Body() body: UserDTO): Promise<string> {
+    return this.appService.createUser(body);
+  }
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.appService.getUsers();
+  }
+
+  @Delete(':id')
+  deleteUser(@Param() { id }: DeleteUserProps): Promise<User> {
+    return this.appService.deleteUser(Number(id));
+  }
+
+  @Put(':id')
+  updateUser(
+    @Body() { user }: PutUserBodyProps,
+    @Param() { id }: PutUserProps,
+  ): Promise<User> {
+    return this.appService.updateUser(Number(id), user);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, User } from '@prisma/client';
+
+import { UserDTO } from './dto/user';
+
+const prisma = new PrismaClient();
 
 @Injectable()
 export class AppService {
-  getHello(id: string): string {
-    return `Hello World ${id}!`;
+  async createUser(user: UserDTO): Promise<string> {
+    const userCreated = await prisma.user.create({
+      data: user,
+    });
+
+    return `${userCreated.email}: ${userCreated.name}`;
+  }
+
+  async getUsers(): Promise<User[]> {
+    return await prisma.user.findMany();
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    return await prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updateUser(id: number, user: User): Promise<User> {
+    const oldUser = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return await prisma.user.update({
+      data: {
+        ...oldUser,
+        ...user,
+      },
+
+      where: {
+        id,
+      },
+    });
   }
 }
